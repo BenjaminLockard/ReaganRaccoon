d9ffce4206c6b72883ae5e080e4bf6ec0b7f39b1
Melody <melodynolastname@gmail.com>
2026-09-17 16:45:46 -0500

    Added Scripting for scene change testing

---
+++ ReaganRaccon/Assets/Scripts/LoadNext.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNext : MonoBehaviour
+{
+    //if object tag has "tag" minigame and pressing action causes the new scene to load
+    private Rigidbody2D rb;
+    private bool isCollidingWithObject;
+    public string miniGameSceneName;
+  
+    private void Awake(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollsionEnter2D(Collision2D collision){
+        if(collision.gameObject.CompareTag("Regan")){
+            isCollidingWithObject = true;
+            SceneManager.LoadScene("SampleScene");
+        }
+        else{
+            isCollidingWithObject = false;
+        }
+        //sets that regan is colliding with the object
+        //allows the player to press the interaction button
+        //within controls
+    }
+
+    private void Update(){
+        if(isCollidingWithObject) // && [interact] is pressed 
+        //transition scene should be loaded as soon as you enter a level
+        //while in the background of that the minigame is being loaded 
+        SceneManager.LoadScene(miniGameSceneName);
+    }
+}
