b49b50a7df6b1e14b8dbce7b65ef08f8b2a90a3c
Melody <melodynolastname@gmail.com>
2026-09-22 13:08:58 -0500

    Added Scripts to this
    Will grab the scripts and push them into main but as of currently this is just me testing

--- ReaganRaccon/Assets/Scripts/LoadNext.cs
+++ ReaganRaccon/Assets/Scripts/LoadNext.cs
@@ -8,23 +8,69 @@ public class LoadNext : MonoBehaviour
     //if object tag has "tag" minigame and pressing action causes the new scene to load
 
     private bool isCollidingWithObject;
-    public string miniGameSceneName;
-
-
-    private void OnTriggerEnter2D(Collider2D collision){
+    public string SceneName;
+    public string type;
+/*
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
         Debug.Log("WE HIT EACH OTTER");
-        if(collision.gameObject.CompareTag("Reagan")){
+        if (collision.gameObject.CompareTag("Reagan"))
+        {
             Debug.Log("WE HIT EACH OTTER");
             isCollidingWithObject = true;
-            SceneManager.LoadScene(miniGameSceneName);
+            if (type == "minigame")
+            {
+                SceneManager.LoadScene(SceneName, LoadSceneMode.Additive);
+            }
+            else
+            {
+                SceneName.LoadScene(SceneName);
+            }
+            //adding additive causes the scene to load on top of each other 
+            //which should allow the main game to be active 
+            //however switching
         }
-        else{
+        else
+        {
             isCollidingWithObject = false;
         }
         //sets that regan is colliding with the object
         //allows the player to press the interaction button
         //within controls
     }
+*/
+    public void switchScene()
+    {
+        if (type == "minigame")
+        {
+            SceneManager.LoadScene(SceneName, LoadSceneMode.Additive);
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+        //adding additive causes the scene to load on top of each other 
+        //which should allow the main game to be active 
+        //however switching to the main game should destory the minigame to not waste data
+        //this can be called 
+    }
+/*
+    //https://docs.unity3d.com/6000.6/Documentation/ScriptReference/SceneManagement.SceneManager.LoadSceneAsync.html
+
+    public void loadMinigame(string name)
+    {
+        StartCorotutine(LoadSceneNext());
+    }
+
+    IEnumerator LoadLevel()
+    {
+    //animation plays here
+    SceneManager.LoadSceneAsync(name);
+    //the main world will not be loaded unfortunately
+    //however im keeping it incase we need to load something in the background
+    //LIKE TRANSITIONING TO AREAS
+    }
+*/
 
     /*private void Update(){
         if(isCollidingWithObject) // && [interact] is pressed 
