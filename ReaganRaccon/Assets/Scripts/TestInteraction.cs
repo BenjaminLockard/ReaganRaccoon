8b57d50b5c58906a6859423f06dc0535567128f0
Melody <melodynolastname@gmail.com>
2026-09-22 16:31:30 -0500

    I scream
    TRYING TO GRAB COMPONENT

--- ReaganRaccon/Assets/Scripts/TestInteraction.cs
+++ ReaganRaccon/Assets/Scripts/TestInteraction.cs
@@ -4,13 +4,17 @@ public class TestInteraction : MonoBehaviour, IInteractable
 {
     private LoadNext loadNext;
     private ReaganInteractions reaganInteractions;
+    private string type;
+    private string name;
     void Start(){
-        loadNext = GetComponent<LoadNext>;
-        reaganInteractions = GetComponent<ReaganInteractions>;
+        loadNext = GetComponent<LoadNext>();
+        reaganInteractions = GetComponent<ReaganInteractions>();
     }
     public void Interact()
     {
-        loadNext.switchScene(SceneType, SceneName);
+        type = ReaganInteractions.GetComponent<string>()["sceneType"];
+        name = ReaganInteractions.GetComponent<string>()["sceneName"];
+        loadNext.switchScene(type, name);
         Debug.Log("SUCCESS! Reagan interacted with Square 2!");
     }
 }
